2b50d153290cf01f117a4b2c46fa353aa6a76118
karen <js785188@gmail.com>
2020-11-30 04:14:46 +0900

    id実装デバッグ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -28,7 +28,7 @@ class LinebotController < ApplicationController
           case event.type
           when Line::Bot::Event::MessageType::Text
             message=CGI.escapeHTML(event.message['text'])
-            Main.new(lineid:event['source']['userId'])
+            Main.new(lineid:event['source']['userId'].to_s)
             if message.match(/^view:/) then
                 res=""
                 n=Main.all.length
