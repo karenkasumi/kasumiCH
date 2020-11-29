0f1993a92084a5f619f55406c6a1bc24c93beb3b
karen <js785188@gmail.com>
2020-11-30 05:31:23 +0900

    id実装デバッグ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -32,7 +32,7 @@ class LinebotController < ApplicationController
                 res=""
                 n=Main.all.length
                 for i in 0..9
-                  res <<"["<< Main.all[n-9+i].id.to_s << ":" << Main.all[n-9+i].content << "]"
+                  res <<"["<< Main.all[n-10+i].id.to_s << ":" << Main.all[n-10+i].content << "]"
                 end
                 message = {
                     type: 'text',
